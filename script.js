55aff677d2c660007ba7cd85315d06f9615b2f4a
Collaborators <66287008+jpotesta10@users.noreply.github.com>
2024-03-19 01:12:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function interactWithPet() {
+    alert("Your AI pet loves the attention!");
+}
+
+function trackMood(mood) {
+    alert(`Glad you're feeling ${mood} today. Your AI pet is always here for you!`);
+}
+
+function startExercise() {
+    alert("Starting a 5-minute mindfulness exercise. Breathe in, breathe out...");
+}
+
+function showReminder() {
+    alert("Don't forget to drink water and stretch a little!");
+}
